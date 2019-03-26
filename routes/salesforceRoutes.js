7fd64225879d74f23e3b53db7ac135b7df7b31e7
Mohit Sharma <mohit@Mohits-MacBook-Air.local>
2019-03-27 00:23:21 +0530

    send result later after promise

--- routes/salesforceRoutes.js
+++ routes/salesforceRoutes.js
@@ -9,13 +9,20 @@ const authHelper = require('../services/authHelper');
 router.get('/callback', function(req, res) {
     
     var sfAuthPromise = authHelper.salesforceAuthorize(req.query.code);
+    var statusCode; 
     sfAuthPromise.then((authData)=>{
         console.log('oauth data ',authData);
         ouath = authData;
-        res.status(200).json({message: 'authorized'});
+        statusCode = 200;
     }).catch((err) => {
-        res.status(401).json({error: 'unauthorized'});
+        statusCode = 401;
     });
+
+    if (statusCode == 200) {
+        res.status(200).json({message: 'authorized'});
+    } else {
+        res.status(401).json({error: 'unauthorized'});
+    }
 });
 
 router.get('/oauth', function(req, res){
