b15411ebfc7246e5a396f164d902e963077d9e9f
karthikkone <iAgilePro@outlook.com>
2019-03-26 15:34:00 +0530

    explicit return response object

--- routes/salesforceRoutes.js
+++ routes/salesforceRoutes.js
@@ -23,14 +23,14 @@ router.get('/callback', function(req, res) {
             if (!err) {
             console.log('Access token : '+resp.access_token);
             oauth = resp;
-            res.status(200).json({message: 'Authorized by Salesforce'});
+                return res.status(200).json({message: 'Authorized by Salesforce'});
             } else {
                 console.log('failed to get access token');
-                res.status(401).json({error: 'Not Authorized by Salesforce'});
+                return res.status(401).json({error: 'Not Authorized by Salesforce'});
             }
         });
     } else {
-        res.status(406).json({error: 'Unexpected request no valid code found'});
+        return res.status(406).json({error: 'Unexpected request no valid code found'});
     }
 });
 
