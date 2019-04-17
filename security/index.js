7364e5a8de0a751ae456cbf92c0213197643b58b
karthikkone <iAgilePro@outlook.com>
2019-04-17 15:32:33 +0530

    added token based authentication middleware

--- security/index.js
+++ security/index.js
@@ -1,7 +1,8 @@
 const accessTokens = require('./tokenManager');
 const userRegistry = require('./userRegistry');
-
+const authFilter = require('./middleware/authFilter')
 module.exports = {
     access: accessTokens,
     auth: userRegistry,
+    authFilter: authFilter,
 }
