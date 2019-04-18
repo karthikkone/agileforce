95e21395c3872d09b45514822302841137ad982c
karthikkone <iAgilePro@outlook.com>
2019-04-18 18:38:18 +0530

    add additional parameter oauth to methods in multi mode orgs

--- salesforce/meta/salesforceMeta.js
+++ salesforce/meta/salesforceMeta.js
@@ -1,28 +1,77 @@
 module.exports = {
 
-    retreiveAndPoll: function retrieveAndPoll(org, retrieveOptions) {
+    retreiveAndPoll: function retrieveAndPoll(org, retrieveOptions, oauth=null) {
+        if (!org) {
+            Promise.reject(new TypeError('org is required'));
+        }
+        if (!org.retrieveOptions) {
+            Promise.reject(new TypeError('retrieveOptions are required'));
+        }
+        if (org.mode == 'multi' && !oauth) {
+            Promise.reject(new TypeError('oauth is required for a multi mode org'));
+        }
+
+        if (org.mode == 'multi') {
+            retrieveOptions.oauth = oauth;
+        }
+
         var retrievePromise = org.meta.retrieveAndPoll(retrieveOptions);
         retrievePromise.poller.on('poll', (pollRes) => { console.log('retrieve poll status: ', pollRes); })
         return retrievePromise;
     },
 
-    validateAndPoll: function validateAndPoll(org,metaZipFile,deployOptions) {
-        var validatePromise = org.meta.deployAndPoll({
-            zipFile: metaZipFile,
-            deployOptions: deployOptions,
-        });
+    validateAndPoll: function validateAndPoll(org,metaZipFile,deployOptions,oauth=null) {
+        let opts = {};
+        if (!org) {
+            Promise.reject(new TypeError('org is required'));
+        }
+        if (!org.deployOptions) {
+            Promise.reject(new TypeError('retrieveOptions are required'));
+        }
+        if (org.mode == 'multi' && !oauth) {
+            Promise.reject(new TypeError('oauth is required for a multi mode org'));
+        }
+
+        if (!metaZipFile) {
+            Promise.reject(new TypeError('metaZipFile is required'));
+        }
+
+        if (org.mode == 'multi') {
+            opts.oauth = oauth;
+        }
+        opts.zipFile = metaZipFile;
+        opts.deployOptions = deployOptions;
+
+        var validatePromise = org.meta.deployAndPoll(opts);
 
         validatePromise.poller.on('poll', (pollRes) => {console.log('validate poll status: ',pollRes);})
         validatePromise.poller.on('done', (pollRes) => {console.log('validate complete: ',pollRes);})
         return validatePromise;
     },
 
-    validateTestAndPoll: function validateTestAndPoll(org,metaZipFile,deployOptions) {
-        var validatePromise = org.meta.deployAndPoll({
-            zipFile: metaZipFile,
-            deployOptions: deployOptions,
-        });
+    validateTestAndPoll: function validateTestAndPoll(org,metaZipFile,deployOptions,oauth=null) {
+        let opts = {};
+        if (!org) {
+            Promise.reject(new TypeError('org is required'));
+        }
+        if (!org.deployOptions) {
+            Promise.reject(new TypeError('retrieveOptions are required'));
+        }
+        if (org.mode == 'multi' && !oauth) {
+            Promise.reject(new TypeError('oauth is required for a multi mode org'));
+        }
 
+        if (!metaZipFile) {
+            Promise.reject(new TypeError('metaZipFile is required'));
+        }
+
+        if (org.mode == 'multi') {
+            opts.oauth = oauth;
+        }
+        opts.zipFile = metaZipFile;
+        opts.deployOptions = deployOptions;
+
+        var validatePromise = org.meta.deployAndPoll(opts);
         validatePromise.poller.on('poll', (pollRes) => {console.log('validate poll status: ',pollRes);})
         validatePromise.poller.on('done', (pollRes) => {console.log('validate complete: ',pollRes);})
         return validatePromise;
