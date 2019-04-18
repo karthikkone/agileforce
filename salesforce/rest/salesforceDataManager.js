26b0329c5e3088740a77e6b8679ba00cd10a3705
karthikkone <iAgilePro@outlook.com>
2019-04-18 15:56:07 +0530

    crud helpers for RemoteAuth

--- salesforce/rest/salesforceDataManager.js
+++ salesforce/rest/salesforceDataManager.js
@@ -1,3 +1,5 @@
+const nforce = require('nforce');
+
 //org is nforce connection object
 module.exports = function(org) {
     var module = {};
@@ -24,8 +26,115 @@ module.exports = function(org) {
         });
     }
 
-    module.addRemoteAuth = function(userSchema,token,oauth=null) {
+    module.getRemoteAuth = function(userSchema,oauth=null) {
         return new Promise((resolve,reject)=>{
+            let q = `SELECT Id, Name, Description__c, Type__c, Token__c FROM RemoteAuth__c WHERE ownerId=${userSchema.forceUserId} LIMIT 1`;
+            let queryOptions = {query: q};
+
+            if (!org) {
+                reject(new TypeError('Org is not defined'));
+            }
+            //oauth is required in multi mode org
+            if (org.mode == 'multi' && oauth == null) {
+                reject(new TypeError('oauth is required in multi mode'));
+            }
+
+            if (!userSchema) {
+                reject(new TypeError('userSchema is required'));
+            }
+
+            if (!userSchema.forceUserId) {
+                reject(new TypeError('forceUserId is required'));
+            }
+
+            if (org.mode == 'multi') {
+                queryOptions.oauth = oauth;
+            }
+
+            org.query(queryOptions, (err, resp)=>{
+                if (!err) {
+                    let rauth = (resp && resp.records && resp.records[0] ? 
+                    resp.records[0] : null); 
+
+                    resolve(rauth);
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    }
+
+    module.addRemoteAuth = function(userSchema,token,oauth=null) {
+        return new Promise((resolve, reject)=>{
+            let dmlOptions = {};
+            if (!org) {
+                reject(new TypeError('Org is not defined'));
+            }
+            //oauth is required in multi mode org
+            if (org.mode == 'multi' && oauth == null) {
+                reject(new TypeError('oauth is required in multi mode'));
+            }
+
+            if (!userSchema) {
+                reject(new TypeError('userSchema is required'))
+            }
+
+            if(!token) {
+                reject(new TypeError('token is required'));
+            }
+
+            if (org.mode == 'multi') {
+                //add oauth in dml options
+                dmlOptions.oauth = oauth;
+            }
+
+             //add required field sObject to dml Options
+            dmlOptions.sobject = nforce.createSObject('RemoteAuth__c',{
+                Description__c: 'agileforce auth @'+ (new Date()),
+                Type__c: 'AgileForce',
+                Token__c: token,
+            });
+            //insert record
+            org.insert(dmlOptions, (err, resp)=>{
+                if (!err) {
+                    reject(err);
+                } else {
+                    console.log('remote auth inserted');
+                    resolve(true);
+                }
+            })
+
+        });
+    }
+
+
+    module.updateRemoteAuth = function(remoteAuth,oauth=null) {
+        return new Promise((resolve, reject)=>{
+            let dmlOptions = {};
+            if (!org) {
+                reject(new TypeError('Org is not defined'));
+            }
+            //oauth is required in multi mode org
+            if (org.mode == 'multi' && oauth == null) {
+                reject(new TypeError('oauth is required in multi mode'));
+            }
+
+            if (org.mode == 'multi') {
+                //add oauth in dml options
+                dmlOptions.oauth = oauth;
+            }
+
+             //add required field sObject to dml Options
+            dmlOptions.sobject = remoteAuth;
+            //insert record
+            org.update(dmlOptions, (err, resp)=>{
+                if (!err) {
+                    reject(err);
+                } else {
+                    console.log('remote auth inserted');
+                    resolve(true);
+                }
+            })
 
         });
     }
