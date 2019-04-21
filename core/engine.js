3420b3ad490545160a471b652f808d8fb9e9fd19
Mohit Sharma <mohit@Mohits-MacBook-Air.local>
2019-04-21 15:30:40 +0530

    use async await

--- core/engine.js
+++ core/engine.js
@@ -2,36 +2,22 @@ const express = require('express');
 
 const config = require('../config');
 const nforce = require('nforce');
-const fs = require('fs');
 const path = require('path');
 const salesforce = require('../salesforce');
 const zipUtil = require('../utils').zipUtil;
 const authManager = require('../salesforce').auth;
-
+const connectedapp = require('../org');
 //load nforce meta-data plugin
 require('nforce-metadata')(nforce);
 
 //constants
-const metahelper = salesforce.meta;
+
 const router = express.Router();
 
-let sfClientId = config.salesforce.clientId;
-let sfClientSecret = config.salesforce.clientSecret;
-let sfRedirectUri = config.salesforce.callBackUri;
 let appWorkpaceRoot = config.app.workspaceRoot;
 
-const org = nforce.createConnection({
-    clientId: sfClientId,
-    clientSecret: sfClientSecret,
-    redirectUri: sfRedirectUri,
-    mode: 'multi',
-    plugins: ['meta'], //load the plugin in this connection
-    metaOpts: {
-        pollInterval: 1000
-    }
-});
+const org = connectedapp.multiModeOrg();
 
-const dataManager = salesforce.rest(org);
 //private methods
 function _parseRetrieveOptions(manifest) {
     let retrieveOptions = {
@@ -53,7 +39,6 @@ function _parseDeployOptions(manifest) {
     let deployOptions = {
         apiVersion: '45.0',
     }
-    let _checkOnly = true;
 
     if (manifest.task == 'deploy') {
         //disable deploy until testing
@@ -72,103 +57,83 @@ function _parseDeployOptions(manifest) {
     return deployOptions;
 }
 
-function _retrieve(manifest,oauth=null) {
+async function _retrieve(manifest, sourceOauth) {
     let retrieveOptions = _parseRetrieveOptions(manifest);
-
-    metahelper.retreiveAndPoll(org,retrieveOptions,oauth).then(function (retResp) {
-        let zipfileName = 'nforce-meta-retrieval-' + retResp.id + '.zip';
-        let metaZipfile = path.join(appWorkpaceRoot, zipfileName);
-        console.log('retrieval: ', retResp.status);
-        console.log('saving retrieval to zip file ', metaZipfile);
-
-        let buf = Buffer.from(retResp.zipFile, 'base64');
-        fs.writeFile(metaZipfile, buf, 'binary', function (err) {
-            if (err) throw err
+    let metaApi = salesforce.meta(org, sourceOauth);
+        metaApi.retreiveAndPoll(retrieveOptions)
+        .then(function (retResp) {
+            var zipfileName = 'nforce-meta-retrieval-' + retResp.id + '.zip';
+            var metaZipLocation = path.join(appWorkpaceRoot, zipfileName);
+            return zipUtil.createZipFrom(retResp.zipFile, metaZipLocation);
+        })
+        .error((err) => {
+            return err;
         });
-        console.log('zip file saved');
-    }).error((err) => {
-        console.error(err);
-    });
 }
 
-function _deploy(manifest,oauth=null,checkOnly=true) {
-    let targetOrgName = manifest.target.org.orgId;
-    let retrievedZipfile;
-    let retrieveOpts = _parseRetrieveOptions(manifest);
-    let deployOpts = _parseDeployOptions(manifest);
+async function _deploy(manifest, sourceOrgOauth, targetOrgOauth,checkOnly=true) {
+    try {
+        let targetMeta = salesforce.meta(org,targetOrgOauth);
+        let deployOptions = _parseDeployOptions(manifest);
+        let metaZipLocation = await _retrieve(manifest, sourceOrgOauth);
+        let metaZipBase64 = await zipUtil.readZipFrom(metaZipLocation, 'base64');
+        
+        if (checkOnly) deployOptions.checkOnly=true;
 
-    if (!deployOpts) {
-        throw new Error('deployOptions is missing');
-    }
-
-    console.log('retreive options: ', retrieveOpts);
-    var targetOrgConn = nforce.createConnection({
-        clientId: sfClientId,
-        clientSecret: sfClientSecret,
-        redirectUri: sfRedirectUri,
-        mode: 'single', //cache oauth in connection object
-        plugins: ['meta'], //load the plugin in this connection
-        metaOpts: {
-            pollInterval: 1000
-        }
-    });
-
-    //authenticate target salesforce org
-    //targetOrgConn.authenticate({username:})
-    console.log('targetOrg in request ', targetOrgName);
-    if (targetOrgName) {
-        metahelper.retreiveAndPoll(org, retrieveOpts,oauth)
-            .then((retResp) => {
-                var zipfileName = 'nforce-meta-retrieval-' + retResp.id + '.zip';
-                var metaZipLocation = path.join(appWorkpaceRoot, zipfileName);
-
-                return zipUtil.createZipFrom(retResp.zipFile, metaZipLocation);
-            })
-            .then((savedZipFilePath) => {
-                retrievedZipfile = savedZipFilePath;
-                
-                return dataManager.getOrg(targetOrgName, 'production',oauth);
-            })
-            .then((targetOrg) => {
-                return authManager.authenticateSingleModeOrg(targetOrgConn,
-                    targetOrg.get('username__c'),
-                    targetOrg.get('password__c'),
-                    targetOrg.get('token__c')
-                );
-            })
-            .then((targetOrgConn) => {
-                return zipUtil.readZipFrom(retrievedZipfile, 'base64');
-            })
-            .then((metaZipBase64) => {
-                return metahelper.validateAndPoll(targetOrgConn,metaZipBase64,deployOpts,oauth);
-            })
-            .then((validateResp) => {
-                console.log('validation status : ', validateResp.status);
-                return validateResp;
-            })
-            .catch((err) => {
-                console.log('retrieveAndValidate operation failed with error : ' + err.message);
-                console.error(err);
-            });
+        targetMeta.deployAndPoll(metaZipBase64,deployOptions);
+    } catch (err) {
+        console.log('build task deploy failed : ',err);
+        return err;
     }
 }
+
 //exports
 module.exports = {
-    build: function (manifest,currentUser) {
 
-        switch (manifest.task) {
-            case 'retrieve':
-                _retrieve(manifest,currentUser.forceOauth);
-                break;
-            case 'validate':
-                _deploy(manifest, currentUser.forceOauth,checkOnly=true);
-                break;
-            case 'deploy':
-                _deploy(manifest,currentUser.forceOauth,checkOnly=true);
-                break;
-            default:
-                throw new Error('Invalid task');
-                break;
+    build: async function (manifest, currentUser) {
+
+        try {
+            let restApi = salesforce.rest(org, oauth);
+            let sourceOrgName = manifest.source.org.orgId;
+            let targetOrgName = manifest.target.org.orgId;
+
+            let sourceOrgOauth;
+            let targetOrgOauth;
+
+            if (sourceOrgName == '__self__') {
+                sourceOrgOauth = currentUser.forceOauth;
+            } else {
+                let sourceOrgData = await restApi.getOrg(sourceOrgName);
+                sourceOrgOauth = await authManager.authenticateMultiModeOrg(org,
+                    sourceOrgData.username__c,
+                    sourceOrgData.password__c,
+                    sourceOrgData.token__c
+                );
+            }
+
+            //get an authenticate target org
+            let targetOrgData = await restApi.getOrg(targetOrgName);
+            targetOrgOauth = await authManager.authenticateSingleModeOrg(org,
+                targetOrgData.username__c,
+                targetOrgData.password__c,
+                targetOrgData.token__c
+            );
+
+
+            switch (manifest.task) {
+                case 'retrieve':
+                    _retrieve(manifest, sourceOrgOauth);
+                    break;
+                case 'deploy':
+                    _deploy(manifest, sourceOrgOauth, targetOrgOauth)
+                    break;
+                case 'validate':
+                    _deploy(manifest, sourceOrgOauth, targetOrgOauth, checkOnly = true);
+                    break;
+            }
+        } catch (err) {
+            console.log('build failed with errors', err);
         }
+
     }
 }
