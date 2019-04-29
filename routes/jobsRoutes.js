645ad493a9e4597bc10705443a0ec53589d4024b
karthikkone <iAgilePro@outlook.com>
2019-04-29 12:34:41 +0530

    parse query param to integer

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const jobs = require('../models/job');
-
+const logger = require('../logger');
 const router = express.Router();
 
 router.get('/:jobId/status', (req, res)=>{
-    var jobId = req.params.jobId;
+    
+    var jobId = parseInt(req.params.jobId);
+    logger.info('finding status of job '+jobId);
     if (!jobId) {
+        logger.error('invalid job id '+jobId);
         return res.status(402).json({error: 'invalid request jobId required'});
     } else {
         let job = jobs.findById(jobId);
+
         if (!job) {
             return res.status(404).json({error: `no job found with Id: ${jobId}`});
         }
