a94301ba19ee0ce2c07cc4b0ab1b4edf78974d30
karthikkone <iAgilePro@outlook.com>
2019-05-03 10:34:40 +0530

    use security filter salesforce rest to find source meta for metadata

--- routes/salesforceRoutes.js
+++ routes/salesforceRoutes.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const config = require('../config');
 const nforce = require('nforce');
-const fs = require('fs');
-const path = require('path');
+
 const salesforce = require('../salesforce');
-const zipUtil = require('../utils').zipUtil;
 const authManager = require('../salesforce').auth;
 const validations = require('../validations');
 const core = require('../core');
@@ -13,6 +11,7 @@ const joi = require('joi');
 const security = require('../security');
 const users = require('../models/user');
 const jobs = require('../models/job');
+const orgManager = require('../org');
 //load nforce meta-data plugin
 //require('nforce-metadata')(nforce);
 
@@ -44,27 +43,27 @@ router.get('/callback', (req, res) => {
             org.getIdentity({ oauth: oauth }, (err, idResp) => {
                 if (!err) {
                     console.log(JSON.stringify(idResp));
-                    let dataManager = salesforce.rest(org,oauth);
+                    let dataManager = salesforce.rest(org, oauth);
                     try {
                         if (!security.auth.isRegisteredUser(idResp.username)) {
                             security.auth.registerUser(idResp.username, oauth);
                         }
                         var existingUser = users.findByUsername(idResp.username);
                         //set oauth token
-                        existingUser.forceOauth=oauth;
-                        existingUser.forceUserId=idResp.user_id;
+                        existingUser.forceOauth = oauth;
+                        existingUser.forceUserId = idResp.user_id;
                         users.updateUser(existingUser);
-                        
+
                         var token = security.access.issue(existingUser);
                         console.log('token issued ', token);
 
                         //save token to connected org for api authentication
-                        dataManager.getRemoteAuth(existingUser,existingUser.forceOauth)
-                        .then((rauth)=>{
-                            return dataManager.upsertRemoteAuth(token,rauth,existingUser.forceOauth);
-                        }).catch((remoteAuthErr)=>{
-                            console.error('adding remote authentication to connected org failed',remoteAuthErr.message);
-                        });
+                        dataManager.getRemoteAuth(existingUser, existingUser.forceOauth)
+                            .then((rauth) => {
+                                return dataManager.upsertRemoteAuth(token, rauth, existingUser.forceOauth);
+                            }).catch((remoteAuthErr) => {
+                                console.error('adding remote authentication to connected org failed', remoteAuthErr.message);
+                            });
 
                     } catch (registrationError) {
                         console.log(registrationError);
@@ -109,170 +108,48 @@ router.get('/orgs', security.authFilter, (req, res) => {
     });
 });
 
-router.get('/meta', isAuthorized, (req, res) => {
-    org.meta.listMetadata({
-        queries: [
-            { type: 'ApexClass' },
-            { type: 'CustomObject' }
-        ]
-    }).then(function (md) {
-        return res.status(200).json(md);
-    }).error(function (err) {
-        return res.status(500).json({ error: 'something went wrong fetching meta data' });
-    });
+router.get('/meta', security.authFilter, (req, res) => {
+    try {
+        let payload = req.body;
+        let currentUser = req.currentUser;
+        if (!payload || !payload.source.org.orgId || payload.queries) {
+            return res.status(401).message({ error: 'invalid request' });
+        }
 
-});
+        let metaApi;
+        if (payload.source.org.orgId == '__self__') {
 
-/*
-router.get('/retrieve', isAuthorized, (req, res) => {
-    metahelper.retreiveAndPoll(org).then(function (retResp) {
-        var zipfileName = 'nforce-meta-retrieval-' + retResp.id + '.zip';
-        var metaZipfile = path.join(appWorkpaceRoot, zipfileName);
-        console.log('retrieval: ', retResp.status);
-        console.log('saving retrieval to zip file ', metaZipfile);
-        console.log('type of zipfile binary retrieved ', typeof (retResp.zipFile));
-        var buf = Buffer.from(retResp.zipFile, 'base64');
-        fs.writeFile(metaZipfile, buf, 'binary', function (err) {
-            if (err) throw err
+            metaApi = salesforce.meta(orgManager.multiModeOrg(), currentUser.forceOauth);
+        } else {
+            let connectedOrg = orgManager.multiModeOrg();
+            let restApi = salesforce.rest(connectedOrg, currentUser.forceOauth);
+            let sourceOrg = await restApi.getOrg(payload.source.org.orgId);
+
+            let sourceOrgOauth = await authManager.authenticateMultiModeOrg(org,
+                sourceOrgData.get('username__c'),
+                sourceOrgData.get('password__c'),
+                sourceOrgData.get('token__c')
+            );
+
+            metaApi = salesforce.meta(sourceOrg, sourceOrgOauth);
+        }
+
+        let mdList = await metaApi.listMetadata({
+            queries: payload.queries
         });
-        console.log('zip file saved');
-    }).error(function (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'failed to fetch and save metadata' })
-    });
-    return res.status(200).json({ message: "metadata retrieved successfully" });
-});
 
-
-router.post('/retrieveAndValidate', isAuthorized, (req, res) => {
-    var targetOrgName = req.body.targetOrgName;
-    var retrievedZipfile;
-    var retrieveOpts = req.body.retrieveOpts;
-    
-    console.log('retreive options: ', retrieveOpts);
-    var targetOrgConn = nforce.createConnection({
-        clientId: sfClientId,
-        clientSecret: sfClientSecret,
-        redirectUri: sfRedirectUri,
-        mode: 'single', //cache oauth in connection object
-        plugins: ['meta'], //load the plugin in this connection
-        metaOpts: {
-            pollInterval: 1000
-        }
-    });
-
-    //authenticate target salesforce org
-    //targetOrgConn.authenticate({username:})
-    console.log('targetOrg in request ',targetOrgName);
-    if (targetOrgName) {
-        metahelper.retreiveAndPoll(org,retrieveOpts)
-            .then((retResp) => {
-                var zipfileName = 'nforce-meta-retrieval-' + retResp.id + '.zip';
-                var metaZipLocation = path.join(appWorkpaceRoot, zipfileName);
-
-                return zipUtil.createZipFrom(retResp.zipFile, metaZipLocation);
-            })
-            .then((savedZipFilePath) => {
-                retrievedZipfile = savedZipFilePath;
-                return dataManager.getOrg(targetOrgName, 'production');
-            })
-            .then((targetOrg) => {
-                return authManager.authenticateSingleModeOrg(targetOrgConn, 
-                    targetOrg.get('username__c'),
-                    targetOrg.get('password__c'),
-                    targetOrg.get('token__c')
-                );
-            })
-            .then((targetOrgConn) => {
-               return zipUtil.readZipFrom(retrievedZipfile, 'base64');
-            })
-            .then((metaZipBase64) => {
-                return metahelper.validateAndPoll(targetOrgConn,metaZipBase64);
-            })
-            .then((validateResp) => {
-                console.log('validation status : ', validateResp.status);
-                return validateResp;
-            })
-            .catch((err) => {
-                console.log('retrieveAndValidate operation failed with error : ' + err.message);
-                console.error(err);
-            });
-
-        return res.status(202).json({ message: 'operation queued' });
-    } else {
-        //missing required params
-        return res.status(406).json({ error: 'missing param targetOrgName' });
+        return res.status(200).json(mdList);
+    } catch (err) {
+        console.log('list metadata failed with errors: ',err);
+        return res.status(500).json({error: err.message});
     }
+
 });
 
-
-router.post('/retrieveTestAndValidate', isAuthorized, (req, res) => {
-    var targetOrgName = req.body.targetOrgName;
-    var retrievedZipfile;
-    var retrieveOpts = req.body.retrieveOpts;
-    var specificTests =req.body.runTests;
-    console.log('retreive options: ', retrieveOpts);
-    console.log(`tests to run ${specificTests}`);
-    
-    var targetOrgConn = nforce.createConnection({
-        clientId: sfClientId,
-        clientSecret: sfClientSecret,
-        redirectUri: sfRedirectUri,
-        mode: 'single', //cache oauth in connection object
-        plugins: ['meta'], //load the plugin in this connection
-        metaOpts: {
-            pollInterval: 1000
-        }
-    });
-
-    //authenticate target salesforce org
-    //targetOrgConn.authenticate({username:})
-    console.log('targetOrg in request ',targetOrgName);
-    if (targetOrgName) {
-        metahelper.retreiveAndPoll(org,retrieveOpts)
-            .then((retResp) => {
-                var zipfileName = 'nforce-meta-retrieval-' + retResp.id + '.zip';
-                var metaZipLocation = path.join(appWorkpaceRoot, zipfileName);
-
-                return zipUtil.createZipFrom(retResp.zipFile, metaZipLocation);
-            })
-            .then((savedZipFilePath) => {
-                retrievedZipfile = savedZipFilePath;
-                return dataManager.getOrg(targetOrgName, 'production');
-            })
-            .then((targetOrg) => {
-                return authManager.authenticateSingleModeOrg(targetOrgConn, 
-                    targetOrg.get('username__c'),
-                    targetOrg.get('password__c'),
-                    targetOrg.get('token__c')
-                );
-            })
-            .then((targetOrgConn) => {
-               return zipUtil.readZipFrom(retrievedZipfile, 'base64');
-            })
-            .then((metaZipBase64) => {
-                return metahelper.validateTestAndPoll(targetOrgConn,metaZipBase64,tests=specificTests);
-            })
-            .then((validateResp) => {
-                console.log('validation status : ', validateResp.status);
-                return validateResp;
-            })
-            .catch((err) => {
-                console.log('retrieveAndValidate operation failed with error : ' + err.message);
-                console.error(err);
-            });
-
-        return res.status(202).json({ message: 'operation queued' });
-    } else {
-        //missing required params
-        return res.status(406).json({ error: 'missing param targetOrgName' });
-    }
-});
-*/
 
 router.post('/build', security.authFilter, (req, res) => {
     let payload = req.body;
-    console.log('API /build current user: ',req.currentUser);
+    console.log('API /build current user: ', req.currentUser);
     if (!payload) {
         return res.status(406).json({ error: 'build manifest is required' });
     }
@@ -285,9 +162,9 @@ router.post('/build', security.authFilter, (req, res) => {
         let buildManifest = parsed.value;
 
         //add job
-        var jobId = jobs.addJob({status:'In progress', task: buildManifest.task});
-        core.build(buildManifest,req.currentUser,jobId);
-        return res.status(201).json({ message: 'operation queued', jobId: jobId});
+        var jobId = jobs.addJob({ status: 'In progress', task: buildManifest.task });
+        core.build(buildManifest, req.currentUser, jobId);
+        return res.status(201).json({ message: 'operation queued', jobId: jobId });
     }
 
 });
