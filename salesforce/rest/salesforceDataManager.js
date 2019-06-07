5155acc3ef5f6c53cd3527a0956a2faa466e42da
karthikkone <iAgilePro@outlook.com>
2019-06-07 13:43:59 +0530

    added logger

--- salesforce/rest/salesforceDataManager.js
+++ salesforce/rest/salesforceDataManager.js
@@ -1,13 +1,14 @@
 const nforce = require('nforce');
-
+const logger = require('../../logger');
 //org is nforce connection object
 module.exports = function (org) {
     var module = {};
     module.getOrg = function (name, type = 'target', oauth = null) {
+        logger.info(`looking for org with Name ${name}`);
         return new Promise((resolve, reject) => {
             let q = `SELECT Id, Name, Type__c, password__c, username__c, token__c FROM Org__c WHERE Name='${name}' AND Type__c='${type}' LIMIT 5`;
             let queryOptions = { query: q };
-
+            
             if (org) {
                 if (org.mode == 'multi') {
                     queryOptions.oauth = oauth;
@@ -27,9 +28,10 @@ module.exports = function (org) {
     }
 
     module.getRemoteAuth = function (userSchema, oauth = null) {
+        
         let q = `SELECT Id, Name, Description__c, Type__c, Token__c FROM RemoteAuth__c WHERE ownerId='${userSchema.forceUserId}' LIMIT 1`;
         let queryOptions = { query: q };
-
+        logger.debug('getRemoteAuth : '+q);
         if (!org) {
             return Promise.reject(new TypeError('Org is not defined'));
         }
@@ -50,7 +52,7 @@ module.exports = function (org) {
             queryOptions.oauth = oauth;
         }
         return new Promise((resolve, reject) => {
-            console.log('finding remote auth object associated with user :', userSchema.username);
+            logger.info('finding remote auth object associated with user :', userSchema.username);
             org.query(queryOptions, (err, resp) => {
                 if (!err) {
                     let rauth = (resp && resp.records && resp.records[0] ?
@@ -102,9 +104,11 @@ module.exports = function (org) {
             console.log('adding a remote auth object')
             org.upsert(dmlOptions, (err, resp) => {
                 if (!err) {
+                    logger.info('failed to add or update RemoteAuth Object')
                     reject(err);
                 } else {
-                    console.log('remote auth inserted');
+                    logger.info('remote auth inserted');
+                    logger.debug('RemoteAuth upsert resultset : '+JSON.stringify(resp));
                     resolve(true);
                 }
             });
